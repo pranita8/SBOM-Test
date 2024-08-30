0ad888653fe59d9e8210f5c4627abedb5d432180
Pranita More <pranita.more.ext@siemens.com>
2024-08-30 16:18:25 +0530

    deleted grandchild

--- child-component-1/grandchild-component/src/main/java/com/example/childcomponent1/GrandchildClass.java
+++
@@ -1,7 +0,0 @@
-package com.example.childcomponent1;
-
-public class GrandchildClass {
-    public void doSomethingGrand() {
-        System.out.println("Doing something grand in Grandchild Component!");
-    }
-}
